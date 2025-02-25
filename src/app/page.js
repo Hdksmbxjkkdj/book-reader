3ba2d820c242aa6e3fe679317b5203733528e67b
ali ghorbani <ag7055408@gmail.com>
2025-02-25 11:36:31 +0330

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import Book from "./components/Book/page";
 import EpubReader from "./components/EpubReader/page";
 
 export default function Home() {
   return <>
-    <Book url={"/pg28885.epub"}/>
     <EpubReader url={"/pg28885.epub"}/>
   </>;
 }
